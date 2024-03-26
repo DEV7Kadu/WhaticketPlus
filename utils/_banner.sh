11ea304ea745223e23283fbb85b5f5df00dc8dc0
carloseduardoback <carloseduardoback@outlook.com>
2024-03-26 13:53:50 -0300

    Update

--- utils/_banner.sh
+++ utils/_banner.sh
@@ -42,7 +42,7 @@ print_banner() {
     print_centered "Compartilhar, vender ou fornecer essa solução" "$Green"
     print_centered "sem autorização é crime previsto no artigo 184" "$Green"
     print_centered "do código penal que descreve a conduta criminosa" "$Green"
-    print_centered "de infringir os direitos autorais da Automatiza AI." "$Green"
+    print_centered "de infringir os direitos autorais do Whats Talk." "$Green"
     print_centered "PIRATEAR ESSA SOLUÇÃO É CRIME." "$Green"
     print_centered "© Whats Talk" "$Green"
 
